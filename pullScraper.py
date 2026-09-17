8f1e9bab9893bd2d31a6b31d0c787ca096e8e4c4
andrew <andrewchen200406@gmail.com>
2026-09-16 20:18:35 -0400

    Added headless option.

--- pullScraper.py
+++ pullScraper.py
@@ -10,7 +10,7 @@ import os
 def initialize_driver(headless=True):
     options = Options()
     if headless:
-        #options.add_argument('--headless=new')
+        options.add_argument('--headless=new')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
